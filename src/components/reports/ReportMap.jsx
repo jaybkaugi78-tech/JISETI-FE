5170d9bbd392eafd9371973c17df3ef3706a0bc0
teresiawangondu <arwatess@gmail.com>
2026-08-20 14:08:47 +0300

    add map cointer to report map

--- src/components/reports/ReportMap.jsx
+++ src/components/reports/ReportMap.jsx
@@ -4,7 +4,7 @@ export default function ReportMap({ report }) {
   const position = [report?.latitude || -1.286389, report?.longitude || 36.817223];
 
   return (
-    <MapContainer center={position} zoom={12} className="map">
+    
       <TileLayer
         attribution='&copy; OpenStreetMap contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
