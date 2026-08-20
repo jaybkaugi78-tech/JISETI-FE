049a79bf26c0090e226a0d229b3272dadeb14a8b
teresiawangondu <arwatess@gmail.com>
2026-08-20 14:40:41 +0300

    Add React Leaflet imports

--- src/components/reports/ReportMap.jsx
+++ src/components/reports/ReportMap.jsx
@@ -1,17 +1,1 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-
-export default function ReportMap({ report }) {
-  const position = [report?.latitude || -1.286389, report?.longitude || 36.817223];
-
-  return (
-    
-      <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>{report?.title || "Report location"}</Popup>
-      </Marker>
-    </MapContainer>
-  );
-}
