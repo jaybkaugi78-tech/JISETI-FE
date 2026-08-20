d72103ba120054f11c03e057b0d4a8e4dc44a7f1
teresiawangondu <arwatess@gmail.com>
2026-08-20 15:38:34 +0300

    Add reports slice and initial state

--- src/features/reports/reportsSlice.js
+++ src/features/reports/reportsSlice.js
@@ -1,1 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialReports = [
+  // your report objects
+];
+
+const reportsSlice = createSlice({
+  name: "reports",
+  initialState: { items: initialReports },
+  reducers: {},
+});
