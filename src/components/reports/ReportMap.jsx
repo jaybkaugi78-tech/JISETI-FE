db2b7ee26c6bf8b3fa047fb486f94c34e517e93d
teresiawangondu <arwatess@gmail.com>
2026-08-20 14:42:31 +0300

    Add map container for reports

--- src/components/reports/ReportMap.jsx
+++ src/components/reports/ReportMap.jsx
@@ -4,4 +4,8 @@ export default function ReportMap({ report }) {
 const position = [
   report?.latitude || -1.286389,
   report?.longitude || 36.817223,
-];
+];return (
+  <MapContainer center={position} zoom={12} className="map">
+  </MapContainer>
+);
+
