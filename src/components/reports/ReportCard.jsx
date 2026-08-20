1ed22ee18b873cc9171c2a90929fe031025ac9eb
teresiawangondu <arwatess@gmail.com>
2026-08-20 13:53:28 +0300

    add report details

--- src/components/reports/ReportCard.jsx
+++ src/components/reports/ReportCard.jsx
@@ -4,10 +4,10 @@ import StatusBadge from "../common/StatusBadge";
 export default function ReportCard({ report }) {
   return (
     <Link to={`/reports/${report.id}`} className="report-card">
-      
+      <div className={`type-icon ${report.type === "Red-Flag" ? "red" : "gold"}`}>""
         {report.type === "Red-Flag" ? "⚑" : "⌂"}
       </div>
-      <div className="report-main">
+      div className="r
         <div className="report-title-row">
           <h3>{report.title}</h3>
           <StatusBadge status={report.status} />
