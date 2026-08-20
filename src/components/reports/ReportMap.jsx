95b324fd2e803d6c9efa2e875d6a9507d99ae861
teresiawangondu <arwatess@gmail.com>
2026-08-20 14:42:01 +0300

    Add report map coordinates

--- src/components/reports/ReportMap.jsx
+++ src/components/reports/ReportMap.jsx
@@ -1,3 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 export default function ReportMap({ report }) {
 }
+const position = [
+  report?.latitude || -1.286389,
+  report?.longitude || 36.817223,
+];
