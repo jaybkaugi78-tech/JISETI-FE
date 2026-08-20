c3fa43c4c36fdbd2d7e3cac60a67aca1ef1c0b63
teresiawangondu <arwatess@gmail.com>
2026-08-20 14:21:11 +0300

    Create authentication slice

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -2,3 +2,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const savedUser = JSON.parse(
   localStorage.getItem("jiseti_user") || "null"
 );
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: savedUser,
+  },
+  reducers: {},
+});
