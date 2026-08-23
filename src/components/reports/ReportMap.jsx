28d069de99f3d6edd4971f2e8a9e305000697218
teresiawangondu <arwatess@gmail.com>
2026-08-23 23:13:47 +0300

    Improve report map coordinates

--- src/components/reports/ReportMap.jsx
+++ src/components/reports/ReportMap.jsx
@@ -1,17 +1,9 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+} from "react-leaflet";
 
-export default function ReportMap({ report }) {
-  const position = [report?.latitude || -1.286389, report?.longitude || 36.817223];
-
-  return (
-    <MapContainer center={position} zoom={12} className="map">
-      <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>{report?.title || "Report location"}</Popup>
-      </Marker>
-    </MapContainer>
-  );
-}
+import { useEffect } from "react";
